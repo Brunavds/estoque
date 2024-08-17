02da57b903901a431845547590e60c1db1fec186
Your Name <you@example.com>
2024-08-17 00:01:25 -0300

    construindo o sistema

---
+++ estoque1_produto.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: estoque1
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
+  `produto_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` varchar(55) NOT NULL,
+  `ctg_produto` varchar(20) NOT NULL,
+  `desc_produto` tinytext NOT NULL,
+  `qtd_produto` varchar(11) NOT NULL,
+  `vlr_compra` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `vlr_venda` decimal(10,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`produto_id`),
+  UNIQUE KEY `produto_id` (`produto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'Casaco Bob','Vestuario','Casaco masculino com capuz, estampa Bob Marley colorido, tamanho M G GG XG. Casaco na cor preta.','100',64.00,159.90);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-16 23:58:40
---
+++ pagina1.py
@@ -0,0 +1,95 @@
+import MySQLdb
+
+def create_connection():
+    try:
+        connection = MySQLdb.connect(
+            host = 'localhost',
+            database = 'estoque1',
+            user = 'root',
+            password = ''
+        )
+        print("Base conectada!")
+    except MySQLdb.Error as e:
+        print(f"Erro ao conectar com a base: {e}")
+        connection = None
+    return connection 
+
+def insert_produto(connection, produto):
+    insert_query = '''
+    INSERT INTO produto (nome_produto, ctg_produto, desc_produto, qtd_produto, vlr_compra,
+    vlr_venda)
+    VALUES (%s, %s, %s, %s, %s, %s ); 
+    '''
+
+    cursor = connection.cursor()
+    try:
+        cursor.execute(insert_query, (
+            produto.nome,
+            produto.categoria,
+            produto.descricao,
+            produto.qtd,
+            produto.valor_compra,
+            produto.valor_venda
+
+        ))
+        connection.commit()
+
+        pergunta = input("Deseja inserir mais produtos? (Sim|Não):  ").strip().lower()
+        if pergunta != 'Sim':
+            print("Produto cadastrado com sucesso.")
+            return False #Para parar o loop pois o break não pode se usado nessa estrutura
+           
+  
+    except MySQLdb.Error as e:
+        print(f"Erro ao cadastrar produto: {e}")
+    finally:
+        cursor.close()
+
+
+class Estoque:
+    # Contrala a operação de adcionar produtos ao banco
+    def __init__(self, connection):
+        self.connection = connection
+
+    def adicionar_produto(self, produto):
+        insert_produto(self.connection, produto)
+
+
+class Produto: 
+    #Produtos que serão incluidos
+    def __init__(self, nome_produto, ctg_produto, desc_produto, qtd_produto, vlr_compra,
+    vlr_venda): 
+        self.nome = nome_produto
+        self.categoria = ctg_produto 
+        self.descricao = desc_produto
+        self.qtd= qtd_produto
+        self. valor_compra= vlr_compra
+        self. valor_venda= vlr_venda
+
+
+def cadastrar_produto(): #Coletar as informações
+    print("****** Sistema de cadastro BY *****")
+    nome = input("Nome do produto: ")
+    categoria = input("Categoria: ")
+    descricao = input ("Descrição: ")
+    qtd = int(input("Quantidade do produto: "))
+    valor_compra = float(input("Valor de compra: "))
+    valor_venda = float(input("Valor de venda: "))
+
+    return Produto(nome, categoria, descricao, qtd, valor_compra, valor_venda)
+
+connection = create_connection()
+
+if connection: 
+    estoque = Estoque(connection)
+
+    while True: 
+        produto = cadastrar_produto()
+        continuar = estoque.adicionar_produto(produto)
+        if continuar is False:
+            break
+
+    
+    connection.close()
+    print("Produto cadastrado com sucesso!")
+
